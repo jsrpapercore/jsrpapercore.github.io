8be1d1ea9f185780f2601691102c782d34a7ef4c
jsrpapercore <92226420+jsrpapercore@users.noreply.github.com>
2021-10-12 19:58:37 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -19,7 +19,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "admin@ThemePure.com";
+        $recipient = "jsrpapercore@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
